41229437b0633405c301b14c1c295eb44c2c95bd
adilsonfuta <adilsonmariafuta@gmail.com>
2019-11-14 09:41:52 +0100

    modificando a zona 3

--- js/eventos.js
+++ js/eventos.js
@@ -1,2 +1,13 @@
 var ex=document.getElementById("rodape");
 
+
+function Soma(a,b){
+    
+    return a+b;
+
+
+}
+
+
+var menu=window.document.getElementsByClassName("exemplo_p").values;
+
---
+++ js/jaesta.ts
@@ -0,0 +1,12 @@
+
+ class Pizzaria{
+
+    private  codigo;
+    private  produ;
+
+
+
+
+
+
+}
